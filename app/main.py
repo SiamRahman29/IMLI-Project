67dedbce82443b3dd9652f2386aceaf12f9d3daf
Siam Rahman <siam@graduate.utm.my>
2025-05-26 17:52:30 +0600

    feat: add cors middleware

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
 # app/main.py
 from fastapi import FastAPI
 from app.routes import routes
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI(
-    title="BARTA-IMLI word of smth smth",
-    description="An API that serves and stores the word of the day, week, month, and any date range.",
+    title="BARTA-IMLI trending words API",
+    description="An API that serves and stores trending word of the day, week, month, and any date range.",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or limit to ["http://localhost:8000"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Include routes
